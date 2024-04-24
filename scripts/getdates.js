f669ffcd449d65df55c96bc02f3510fdc84147fe
BrandonMHerrin <brandon.herrin@live.com>
2024-04-23 20:44:39 -0600

    week1: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year and last modified date and updates the footer
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+document.getElementById("currentyear").innerText = currentYear;
+document.getElementById('lastModified').innerText = document.lastModified;
